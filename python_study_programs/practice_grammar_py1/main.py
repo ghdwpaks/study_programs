cfdbce25673d54f9a4481d07fde82050fdb6a8fc
ghdwpaks <hjm7904@gmail.com>
2021-04-20 23:57:42 +0900

    P.G.P 7 'modified for moduling' 구조화 편의성 때문에 ui 약간 수정함

--- python_study_programs/practice_grammar_py1/main.py
+++ python_study_programs/practice_grammar_py1/main.py
@@ -23,56 +23,49 @@ def setting_string() :
 
 def test_slicing() :
     global str_t
-    for i in range (0,10) :
-        setting_string()
-        os.system("cls")
-        print("원문은 str_t : {} 입니다.".format(str_t))
-        r1 = random.randint(1,3)
-        if r1 == 1:
-            #[r2:]
-            r2 = random.randint(-len(str_t),len(str_t)-1)
-            print("str_t[{}:] 은 뭘까요?".format(r2))
-            ans = input("")
-            check_ans(ans,str_t[r2:])
-        elif r1 == 2 :
-            #[r2:r3]
-            r2 = random.randint(-len(str_t),len(str_t))
-            r3 = 0
-            if r2 < 0 :
-                r3 = random.randint(len(str_t)+r2+1,len(str_t))
-            else :
-                r3 = random.randint(r2+1,len(str_t))
-            print("str_t[{}:{}] 은 뭘까요?".format(r2,r3))
-            ans = input("")
-            check_ans(ans,str_t[r2:r3])
-        elif r1 == 3 :
-            #[:r2]
-            r2 = random.randint(-len(str_t)+1,len(str_t))
-            print("str_t[:{}] 은 뭘까요?".format(r2))
-            ans = input("")
-            check_ans(ans,str_t[:r2])
-        setting_string()
+    os.system("cls")
+    print("원문은 str_t : {} 입니다.".format(str_t))
+    r1 = random.randint(1,3)
+    if r1 == 1:
+        #[r2:]
+        r2 = random.randint(-len(str_t),len(str_t)-1)
+        print("str_t[{}:] 은 뭘까요?".format(r2))
+        ans = input("")
+        check_ans(ans,str_t[r2:])
+    elif r1 == 2 :
+        #[r2:r3]
+        r2 = random.randint(-len(str_t),len(str_t))
+        r3 = 0
+        if r2 < 0 :
+            r3 = random.randint(len(str_t)+r2+1,len(str_t))
+        else :
+            r3 = random.randint(r2+1,len(str_t))
+        print("str_t[{}:{}] 은 뭘까요?".format(r2,r3))
+        ans = input("")
+        check_ans(ans,str_t[r2:r3])
+    elif r1 == 3 :
+        #[:r2]
+        r2 = random.randint(-len(str_t)+1,len(str_t))
+        print("str_t[:{}] 은 뭘까요?".format(r2))
+        ans = input("")
+        check_ans(ans,str_t[:r2])
+    setting_string()
 
 def test_length() :
     global str_t
-    for i in range(0,10) :
-        setting_string()
-        print("원문은 str_t : {} 입니다.".format(str_t))
-        print("len(str_t) 은 뭘까요?")
-        ans = input("")
-        check_ans(ans,str(len(str_t)))
+    print("원문은 str_t : {} 입니다.".format(str_t))
+    print("len(str_t) 은 뭘까요?")
+    ans = input("")
+    check_ans(ans,str(len(str_t)))
 
 def test_count() :
     global str_t
-    for i in range(0,10) :
-        setting_string()
-        print("원문은 str_t : {}입니다.".format(str_t))
-        chr1 = str_t[random.randint(0,len(str_t)-1)]
-        print("str_t.count('{}')은 뭘까요?".format(chr1))
-        ans = input("")
-        check_ans(ans,chr1)
+    print("원문은 str_t : {}입니다.".format(str_t))
+    chr1 = str_t[random.randint(0,len(str_t)-1)]
+    print("str_t.count('{}')은 뭘까요?".format(chr1))
+    ans = input("")
+    check_ans(ans,chr1)
 
-        
 
 def check_ans(user_ans , right_ans) :
     if user_ans == right_ans :
@@ -98,33 +91,35 @@ while True :
     print("8.rfind 문자열에서 인자를 오른쪽부터 찾고 그 위치 알려주기")
     print("9.slicing 문자열 슬라이싱 테스트 진입")
     userans1 = int(input("입력 : "))
-    if userans1 == 1 :
-        #len, 글자 수 세기
-        test_length()
-    elif userans1 == 2 :
-        #count 문자열에서 인자갯수 찾기
-        print(2)
-    elif userans1 == 3 :
-        #upper 모두 대문자로 변환하기
-        print(3)
-    elif userans1 == 4 :
-        #lower 모두 소문자로 변환하기
-        print(4)
-    elif userans1 == 5 :
-        #strip 왼쪽, 오른쪽 모두 공백 없애기
-        print(5)
-    elif userans1 == 6 :
-        #replace 문자열 대체하기
-        print(6)
-    elif userans1 == 7 :
-        #find 문자열에서 인자를 왼쪽부터 찾고 그 위치 알려주기
-        print(7)
-    elif userans1 == 8 :
-        #rfind 문자열에서 인자를 오른쪽부터 찾고 그 위치 알려주기
-        print(8)
-    elif userans1 == 9 :
-        #find 찾지 못하면 -1 리턴
-        print(9)
-    elif userans1 == 10 :
-        print("문자열 슬라이싱 테스트 진입")
-        test_slicing()
+    for i in range(0,10) :
+        setting_string()
+        if userans1 == 1 :
+            #len, 글자 수 세기
+            test_length()
+        elif userans1 == 2 :
+            #count 문자열에서 인자갯수 찾기
+            print(2)
+        elif userans1 == 3 :
+            #upper 모두 대문자로 변환하기
+            print(3)
+        elif userans1 == 4 :
+            #lower 모두 소문자로 변환하기
+            print(4)
+        elif userans1 == 5 :
+            #strip 왼쪽, 오른쪽 모두 공백 없애기
+            print(5)
+        elif userans1 == 6 :
+            #replace 문자열 대체하기
+            print(6)
+        elif userans1 == 7 :
+            #find 문자열에서 인자를 왼쪽부터 찾고 그 위치 알려주기
+            print(7)
+        elif userans1 == 8 :
+            #rfind 문자열에서 인자를 오른쪽부터 찾고 그 위치 알려주기
+            print(8)
+        elif userans1 == 9 :
+            #find 찾지 못하면 -1 리턴
+            print(9)
+        elif userans1 == 10 :
+            print("문자열 슬라이싱 테스트 진입")
+            test_slicing()
