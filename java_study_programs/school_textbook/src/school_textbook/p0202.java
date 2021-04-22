637175ef4dcfc90aab3d04420890ad9a813a3245
ghdwpaks <hjm7904@gmail.com>
2021-04-23 00:48:45 +0900

    S.T. from 18 to 27

---
+++ java_study_programs/school_textbook/src/school_textbook/p0202.java
@@ -0,0 +1,15 @@
+package school_textbook;
+
+public class p0202 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		byte b = (byte)a;
+		b = (byte)(a);
+		
+		
+		
+
+	}
+
+}
---
+++ java_study_programs/school_textbook/src/school_textbook/p0242.java
@@ -0,0 +1,13 @@
+package school_textbook;
+
+public class p0242 {
+
+	public static void main(String[] args) {
+		int[] a = new int[]{1,2,3,4,5,6,7,8,9,10};
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("a["+i+"] = "+a[i]);
+		}
+
+	}
+
+}
