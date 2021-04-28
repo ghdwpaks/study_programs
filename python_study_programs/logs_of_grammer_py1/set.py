06036417ee89bcc7e2b180481b67adefc35c37e5
ghdwpaks <hjm7904@gmail.com>
2021-04-28 19:44:43 +0900

    L.O.G 4 update set 'remove and discard' set 형식의 remove 함수와 discard 함수 추가 작성

--- python_study_programs/logs_of_grammer_py1/set.py
+++ python_study_programs/logs_of_grammer_py1/set.py
@@ -19,6 +19,28 @@ games.add("R6S")
 print(games)
 #add 는 요소 하나만 추가할때 쓰는것이고,
 
+games.remove('R6S')
+print(games)
+#remove는 요소를 제거할때 쓴다.
+'''
+games.remove("most_game","hate_game")
+print(games)
+'''
+#다만 두개를 한꺼번에 지울 순 없다.
+
+games.discard('GTA5')
+#지금 위 코드를 보면 games라는 셋에는 없는 'GTA5'라는 요소를 지우라고 명령이 나와있는데, 이럼에도 불구하고
+print(games)
+#오류가 뜨지 않는다.
+'''
+discard는 remove와는 다르게 굉장히 유연하기 때문에
+요소가 제거됨은 확신하기 위해서 쓰는 경우가 많다고 한다.
+따라서 위의 구문의 의미는
+'GTA5'라는 요소가 확실히 사라졌는지 알 수 없기에
+games에 다시 한번 들어가서 'GTA5'라는 요소가 사라졌는지
+확인한것이라고 보면 된다.
+'''
+
 games.update(("UTS","EFT"))
 print(games)
 #update는 요소 여러개를 추가할때 쓰는것이다.
